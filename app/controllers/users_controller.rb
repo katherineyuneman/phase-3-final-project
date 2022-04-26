f21e2d35b581c70cd90857cb23ccad4e95ba27ca
katherineyuneman <kyuneman@gmail.com>
2022-04-26 13:15:50 -0400

    Adds error messages for validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,16 @@ class UsersController < ApplicationController
     
     
     post '/users' do
-        @user = User.create(
+        user = User.create(
             first_name: params[:first_name],
             last_name: params[:last_name],
             email: params[:email]
         )
-        @user.to_json
+        if user.save
+            user.to_json
+        else
+            user.errors.full_messages.to_sentence
     end
+end
 
 end
